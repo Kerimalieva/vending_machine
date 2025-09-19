9b0d4f860dbb3b3c58c42e005faf4adb7ecdbed7
zarinakerimalieva <kerimalieva.zarina.2003@gmail.com>
2025-09-19 17:51:59 +0600

    реализовала методы интерфейса PaymentAcceptor для оплаты монетами

--- src/payment/CoinAcceptor.java
+++ src/payment/CoinAcceptor.java
@@ -1,6 +1,7 @@
 package payment;
 
-public class CoinAcceptor {
+
+public class CoinAcceptor implements PaymentAcceptor{
     private int amount;
 
     public CoinAcceptor(int amount) {
@@ -14,4 +15,26 @@ public class CoinAcceptor {
     public void setAmount(int amount) {
         this.amount = amount;
     }
+
+    @Override
+    public boolean canPay(int price) {
+        return amount>=price;
+    }
+
+    @Override
+    public boolean pay(int price) {
+        if (canPay(price)) {
+            this.amount -= price;
+            System.out.println("Платеж принят. Списано: " + price);
+            return true;
+        }
+        System.out.println("Недостаточно средств.");
+        return false;
+    }
+
+    @Override
+    public void displayStatus() {
+        System.out.println("Монет на сумму: " + amount);
+    }
 }
+
